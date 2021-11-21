0eb749a93ea5d004bbb77b2da9b1db4b7cf3e8d3
Jai Arora <infolinesoni@gmail.com>
2021-11-21 22:50:27 +0530

    Fixed TimeExample3

---
+++ TimeExample3/.time3.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/airj23/Documents/ModularProgramming/TimeExample3/time3.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/airj23/Documents/ModularProgramming/TimeExample3/time3.ko /home/airj23/Documents/ModularProgramming/TimeExample3/time3.o /home/airj23/Documents/ModularProgramming/TimeExample3/time3.mod.o ;  true
---
+++ TimeExample3/.tmp_versions/time3.mod
@@ -0,0 +1,3 @@
+/home/airj23/Documents/ModularProgramming/TimeExample3/time3.ko
+/home/airj23/Documents/ModularProgramming/TimeExample3/time3.o
+
